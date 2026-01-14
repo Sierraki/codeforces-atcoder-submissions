d9e579df29b7f7bc8ec1fc47a9e3a5fe037e9286
Enter <Enter>
2026-01-14 14:07:00 +0600

    Add solution for problem `A - Product` Link: https://atcoder.jp/contests/abc086/tasks/abc086_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc086/submissions/72452431

--- submissions/atcoder/abc086/abc086A Product.py
+++ submissions/atcoder/abc086/abc086A Product.py
@@ -1,23 +1,9 @@
-from collections import defaultdict, Counter, deque
-from math import sqrt, floor
-from bisect import bisect, bisect_left
-from itertools import accumulate as acc
 import sys
-
-input = sys.stdin.readline
-def mii():
-    return map(int, input().split())
-def lmii():
-    return list(map(int, input().split()))
-def ii():
-    return int(input())
-def si():
-    return input()[:-1]
-def lacc(nums):
-    return list(acc(nums))
-
-a, b = mii()
-if a*b % 2 == 0:
-    print("Even")
+input=sys.stdin.readline
+data=iter(input().split())
+a, b = int(next(data)), int(next(data))
+res=a*b
+if res%2==0:
+    print('Even')
 else:
-    print("Odd")
+    print('Odd')

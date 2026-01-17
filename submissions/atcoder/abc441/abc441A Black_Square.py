cc17474ef7cf9424a0d6679dfbfefafb628b022b
Enter <Enter>
2026-01-17 18:06:00 +0600

    Add solution for problem `A - Black Square` Link: https://atcoder.jp/contests/abc441/tasks/abc441_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc441/submissions/72507573

--- submissions/atcoder/abc441/abc441A Black_Square.py
+++ submissions/atcoder/abc441/abc441A Black_Square.py
@@ -1,9 +1,15 @@
 import sys
 
 input = sys.stdin.readline
+# data = iter(sys.stdin.read().split())
+
 p, q = map(int, input().split())
 x, y = map(int, input().split())
-if p <= x <= p + 99 and q <= y <= q+ 99 :
+
+a = p + 100 - 1 if p + 100 - 1 >= 0 else 0
+b = q + 100 - 1 if q + 100 - 1 >= 0 else 0
+
+if p <= x <= a and q <= y <= b:
     print("Yes")
 else:
     print("No")

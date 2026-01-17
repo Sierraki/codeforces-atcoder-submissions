f7bd3b5bfacb7a25ddd80f175853d6464f15853e
Enter <Enter>
2026-01-17 18:39:00 +0600

    Add solution for problem `C - Sake or Water` Link: https://atcoder.jp/contests/abc441/tasks/abc441_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc441/submissions/72534519

--- submissions/atcoder/abc441/abc441C Sake_or_Water.py
+++ submissions/atcoder/abc441/abc441C Sake_or_Water.py
@@ -1,11 +1,26 @@
 import sys
 
 input = sys.stdin.readline
+# data = iter(sys.stdin.read().split())
+# size = int(input())
+# for _ in range(size):
+#     n=int(input())
 n, k, x = map(int, input().split())
 nums = list(map(int, input().split()))
+
+# print(n, k, x)
+# print(nums)
+
 nums.sort(reverse=True)
+total = sum(nums)
+water = sum(nums[: (n - k)])
+
+wine = total - water
+
+# print(water, wine)
+
 cnt = n - k
-if sum(nums) - sum(nums[: (n - k)]) >= x:
+if wine >= x:
     pin = n - k
     while x > 0:
         x -= nums[pin]

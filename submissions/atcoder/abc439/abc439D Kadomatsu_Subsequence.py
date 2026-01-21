6d510aeded241fa13ae81a86886060f839d749ce
Enter <Enter>
2026-01-21 11:59:00 +0600

    Add solution for problem `D - Kadomatsu Subsequence` Link: https://atcoder.jp/contests/abc439/tasks/abc439_d Tags: AtCoder, *425 Ref: https://atcoder.jp/contests/abc439/submissions/72620920

--- submissions/atcoder/abc439/abc439D Kadomatsu_Subsequence.py
+++ submissions/atcoder/abc439/abc439D Kadomatsu_Subsequence.py
@@ -22,48 +22,20 @@ def si():
     return input()[:-1]
 
 
-from collections import defaultdict, Counter, deque
-from math import sqrt, floor
-from bisect import bisect, bisect_left
-import sys
-
-input = sys.stdin.readline
-
-
-def mii():
-    return map(int, input().split())
-
-
-def lmii():
-    return list(map(int, input().split()))
-
-
-def ii():
-    return int(input())
-
-
-def si():
-    return input()[:-1]
-
-
 n = ii()
 nums = lmii()
-ans = 0
-cnt = Counter()
-total = Counter(nums)
-for i in range(n):
-    cur = nums[i]
 
-    cnt[cur] += 1
-    total[cur] -= 1
-    
 
-    
+def fun(nums):
+    ans = 0
+    cnt = Counter()
+    for i in range(n):
+        cnt[nums[i]] += 1
+        if i >= 2:
+            cur = nums[i]
+            if cur % 5 == 0:
+                if cur // 5 * 3 in cnt and cur // 5 * 7 in cnt:
+                    ans += cnt[cur // 5 * 3] * cnt[cur // 5 * 7]
+    return ans
 
-    if cur % 5 == 0:
-        if cur // 5 * 3 in cnt and cur // 5 * 7 in cnt:
-            ans += cnt[cur // 5 * 3] * cnt[cur // 5 * 7]
-        if cur // 5 * 3 in total and cur // 5 * 7 in total:
-            ans += total[cur // 5 * 3] * total[cur // 5 * 7]
-
-print(ans)
+print(fun(nums)+fun(nums[::-1]))

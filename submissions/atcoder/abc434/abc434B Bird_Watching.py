6dcf006fd2d969e6bfa9f488d852101bc6b4a9ca
Enter <Enter>
2026-01-23 13:54:00 +0600

    Add solution for problem `B - Bird Watching` Link: https://atcoder.jp/contests/abc434/tasks/abc434_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc434/submissions/72654682

--- submissions/atcoder/abc434/abc434B Bird_Watching.py
+++ submissions/atcoder/abc434/abc434B Bird_Watching.py
@@ -5,30 +5,22 @@ from itertools import accumulate
 import sys
 
 input = sys.stdin.readline
-
-
 def mii():
     return map(int, input().split())
-
-
 def lmii():
     return list(map(int, input().split()))
-
-
 def ii():
     return int(input())
-
-
 def si():
     return input()[:-1]
 
 
-n, m = mii()
-cnt = defaultdict(list)
-for i in range(1, m + 1):
-    cnt[i] = []
+n,m=mii()
+cnt=defaultdict(list)
 for _ in range(n):
-    a, b = mii()
+    a,b=mii()
     cnt[a].append(b)
-for i in cnt.values():
-    print(sum(i) / len(i))
+res=[[i, (sum(j)/len(j))] for i,j in cnt.items()]
+res.sort()
+for i,j in res:
+    print(j)

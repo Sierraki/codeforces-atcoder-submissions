7969244f356683551182d4d67dab5ee00b294bf6
Enter <Enter>
2026-01-24 18:01:00 +0600

    Add solution for problem `A - Count .` Link: https://atcoder.jp/contests/abc442/tasks/abc442_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc442/submissions/72675652

---
+++ submissions/atcoder/abc442/abc442A Count_..py
@@ -0,0 +1,35 @@
+from collections import defaultdict, Counter, deque
+from math import sqrt, floor
+from bisect import bisect, bisect_left
+from itertools import accumulate as acc
+import sys
+
+input = sys.stdin.readline
+
+
+def mii():
+    return map(int, input().split())
+
+
+def lmii():
+    return list(map(int, input().split()))
+
+
+def ii():
+    return int(input())
+
+
+def si():
+    return input()[:-1]
+
+
+def lacc(nums):
+    return list(acc(nums))
+
+
+s = si()
+ans = 0
+for i in s:
+    if i in "ij":
+        ans += 1
+print(ans)

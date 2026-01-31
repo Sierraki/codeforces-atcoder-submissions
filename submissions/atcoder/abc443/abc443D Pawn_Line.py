e4b7853300d706ba5e3f826898c919cfffd39395
Enter <Enter>
2026-01-31 18:54:00 +0600

    Add solution for problem `D - Pawn Line` Link: https://atcoder.jp/contests/abc443/tasks/abc443_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc443/submissions/72895936

--- submissions/atcoder/abc443/abc443D Pawn_Line.py
+++ submissions/atcoder/abc443/abc443D Pawn_Line.py
@@ -1,50 +1,41 @@
 from collections import defaultdict, Counter, deque
-from math import sqrt, floor, gcd, ceil
+from math import sqrt, floor,gcd,ceil
 from bisect import bisect, bisect_left
 from itertools import accumulate as acc
-from functools import cache, lru_cache
-import heapq
 import sys
 
 input = sys.stdin.readline
-
-
 def mii():
     return map(int, input().split())
-
-
 def lmii():
     return list(map(int, input().split()))
-
-
 def ii():
     return int(input())
-
-
 def si():
     return input().strip()
-
-
 def lacc(nums):
     return list(acc(nums))
 
 
-def ms(numss):
-    return "".join(map(str, numss))
-
-
 def solve():
-    n = ii()
-    nums = lmii()
+    n=ii()
+    nums=lmii()
 
-    total = sum(nums)
-    total = sum(nums)
+    # -->
+    left = [0] * n
+    left[0] = nums[0]
     for i in range(1, n):
-        nums[i] = min(nums[i], nums[i - 1] + 1)
+        left[i] = min(nums[i], left[i-1] + 1)
+    # <--
+    right = [0] * n
+    right[-1] = nums[-1]
     for i in range(n - 2, -1, -1):
-        nums[i] = min(nums[i], nums[i + 1] + 1)
-    print( total -sum(nums))
-
+        right[i] = min(nums[i], right[i+1] + 1)
+    ans = 0
+    for i in range(n): 
+        res = min(left[i], right[i])
+        ans += (nums[i] - res)
+    print(ans)
 
 # sys.setrecursionlimit(200000)
 if __name__ == "__main__":

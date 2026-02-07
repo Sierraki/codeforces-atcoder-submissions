995f705038c19767489d88b374f0986bd6c59f46
Enter <Enter>
2026-02-07 15:59:00 +0600

    Add solution for problem `B - Permute to Minimize` Link: https://atcoder.jp/contests/abc432/tasks/abc432_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc432/submissions/73041644

---
+++ submissions/atcoder/abc432/abc432B Permute_to_Minimize.py
@@ -0,0 +1,57 @@
+from collections import defaultdict, Counter, deque
+from math import sqrt, floor, gcd, ceil
+from bisect import bisect, bisect_left
+from itertools import accumulate as acc
+import sys
+
+input = sys.stdin.readline
+
+
+def mii():
+    return map(int, input().split())
+
+
+def lmii():
+    return list(map(int, input().split()))
+
+
+def ii():
+    return int(input())
+
+
+def si():
+    return input().strip()
+
+
+def lacc(nums):
+    return list(acc(nums))
+
+
+def ms(numss):
+    return "".join(map(str, numss))
+
+
+def solve():
+    num = si()
+    zero = 0
+    res = []
+    for i in num:
+        if i == "0":
+            zero += 1
+        else:
+            res.append(int(i))
+    res.sort()
+    if zero == 0:
+        print(ms(res))
+    else:
+        ans = ms([res[0]] + [0] * zero)
+        if res[1:]:
+            ans += ms(res[1:])
+        print(ans)
+
+
+# sys.setrecursionlimit(200000)
+if __name__ == "__main__":
+    # size = ii()
+    # for _ in range(size):
+    solve()

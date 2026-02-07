383c7c048ac143f3760e98779d132e467b77a7e2
Enter <Enter>
2026-02-07 18:07:00 +0600

    Add solution for problem `B - Digit Sum` Link: https://atcoder.jp/contests/abc444/tasks/abc444_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc444/submissions/73056879

---
+++ submissions/atcoder/abc444/abc444B Digit_Sum.py
@@ -0,0 +1,56 @@
+from collections import defaultdict, Counter, deque
+from math import sqrt, floor, gcd, ceil
+from bisect import bisect, bisect_left
+from itertools import accumulate as acc
+import sys
+
+input = sys.stdin.readline
+
+
+def mii():
+    return map(int, input().split())
+
+
+def lmii():
+    return list(map(int, input().split()))
+
+
+def ii():
+    return int(input())
+
+
+def si():
+    return input().strip()
+
+
+def lacc(nums):
+    return list(acc(nums))
+
+
+def ms(numss):
+    return "".join(map(str, numss))
+
+
+def solve():
+    n, k = mii()
+
+    def check(num):
+        cnt = 0
+        for i in range(len(str(num))):
+            cnt += int(str(num)[i])
+            if cnt > k:
+                return False
+        return cnt == k
+
+    ans = 0
+    for i in range(n + 1):
+        if check(i):
+            ans += 1
+    print(ans)
+
+
+# sys.setrecursionlimit(200000)
+if __name__ == "__main__":
+    # size = ii()
+    # for _ in range(size):
+    solve()

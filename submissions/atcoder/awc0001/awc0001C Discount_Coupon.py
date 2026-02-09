4d69783811bd89babfe0c194b8f77db5cff4a28f
Enter <Enter>
2026-02-09 17:10:00 +0600

    Add solution for problem `C - Discount Coupon` Link: https://atcoder.jp/contests/awc0001/tasks/awc0001_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/awc0001/submissions/73158586

---
+++ submissions/atcoder/awc0001/awc0001C Discount_Coupon.py
@@ -0,0 +1,48 @@
+from collections import defaultdict, Counter, deque
+from math import sqrt, floor, gcd, ceil
+from bisect import bisect, bisect_left
+from itertools import accumulate as acc
+import sys
+
+input = sys.stdin.readline
+
+
+def mii():
+    return map(int, input().split())
+
+
+def lmii():
+    return list(map(int, input().split()))
+
+
+def ii():
+    return int(input())
+
+
+def si():
+    return input().strip()
+
+
+def lacc(nums):
+    return list(acc(nums))
+
+
+def ms(numss):
+    return "".join(map(str, numss))
+
+
+def solve():
+    n, k = mii()
+    nums = lmii()
+    nums.sort(reverse=True)
+    if k >= len(nums):
+        print(0)
+    else:
+        print(sum(nums[k:]))
+
+
+# sys.setrecursionlimit(200000)
+if __name__ == "__main__":
+    # size = ii()
+    # for _ in range(size):
+    solve()

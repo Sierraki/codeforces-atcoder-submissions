c0f6685fa2ef25becb9f8a2a9185256a26a2ea2e
Enter <Enter>
2026-02-14 00:58:00 +0600

    Add solution for problem `A - Reward of Multiples` Link: https://atcoder.jp/contests/awc0005/tasks/awc0005_a Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/awc0005/submissions/73270006

---
+++ submissions/atcoder/awc0005/awc0005A Reward_of_Multiples.py
@@ -0,0 +1,51 @@
+from collections import defaultdict, Counter, deque
+from math import sqrt, floor, gcd, ceil
+from bisect import bisect, bisect_left
+from itertools import accumulate as acc
+from functools import cache, lru_cache
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+
+def mii():
+    return map(int, input().split())
+
+
+def lmii():
+    return list(map(int, input().split()))
+
+
+def ii():
+    return int(input())
+
+
+def si():
+    return input().strip()
+
+
+def lacc(nums):
+    return list(acc(nums))
+
+
+def ms(numss):
+    return "".join(map(str, numss))
+
+
+def solve():
+    n, k = mii()
+    nums = lmii()
+
+    ans = 0
+    for i in nums:
+        if i % k == 0:
+            ans += i
+    print(ans)
+
+
+# sys.setrecursionlimit(200000)
+if __name__ == "__main__":
+    # size = ii()
+    # for _ in range(size):
+    solve()

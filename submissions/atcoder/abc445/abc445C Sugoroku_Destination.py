7fb37d7900b2fc30427253621d450a388e3f0ebe
Enter <Enter>
2026-02-14 19:22:00 +0600

    Add solution for problem `C - Sugoroku Destination` Link: https://atcoder.jp/contests/abc445/tasks/abc445_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc445/submissions/73323308

--- submissions/atcoder/abc445/abc445C Sugoroku_Destination.py
+++ submissions/atcoder/abc445/abc445C Sugoroku_Destination.py
@@ -36,38 +36,13 @@ def ms(numss):
 def solve():
     n = ii()
     nums = lmii()
-    adj = [[] for _ in range(n + 1)]
-    # print(nums)
-
-    for i, j in enumerate(nums, 1):
-        adj[j].append(i)
-
-    # print(adj)
-    res = []
-    vis = [False] * (n + 1)
-
-    def dfs(cur, path):
-        if not vis[cur]:
-            path.append(cur)
-            vis[cur] = True
-            for i in adj[cur]:
-                dfs(i, path)
-
-    for i in nums[::-1]:
-        if not vis[i]:
-            path = []
-            dfs(i, path)
-            res.append(path[:])
-
-    # print(res)
-    ans = [0] * (n + 1)
-    for i in res:
-        for j in i:
-            ans[j] = i[0]
-    print(*ans[1:])
+    for i in range(n - 1,-1, -1): 
+        tar = nums[i] - 1 
+        nums[i] = nums[tar]
+    print(*nums)
 
 
-sys.setrecursionlimit(500000)
+sys.setrecursionlimit(10**5)
 if __name__ == "__main__":
     # size = ii()
     # for _ in range(size):

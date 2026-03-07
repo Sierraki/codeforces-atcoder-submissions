016baf3e1a20f38712872d515e8dc97ef7ef0935
Enter <Enter>
2026-03-08 00:12:00 +0600

    Add solution for problem `B - mpp` Link: https://atcoder.jp/contests/abc447/tasks/abc447_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc447/submissions/73942810

---
+++ submissions/atcoder/abc447/abc447B mpp.py
@@ -0,0 +1,53 @@
+import heapq
+import sys
+from bisect import bisect, bisect_left
+from collections import Counter, defaultdict, deque
+from functools import cache, lru_cache
+from itertools import accumulate as acc
+from math import ceil, floor, gcd, sqrt
+
+input = sys.stdin.readline
+
+# num
+def ii(): return int(input())
+def mii(): return map(int, input().split())
+def lmii(): return list(map(int, input().split()))
+# string
+def lmsi(): return list(map(str, si()))
+def si(): return input().strip()
+def ms(numss): return "".join(map(str, numss))
+# else
+def lacc(nums): return list(acc(nums))
+def matt(row, col): return [[0] * col for _ in range(row)]
+def p(numss):
+    for i in numss:
+        print(i)
+def read_mat(n): return [lmii() for _ in range(n)]
+
+
+
+def solve():
+
+    s = lmsi()
+
+    cnt = Counter(s)
+
+    tar = max(cnt.values())
+
+    pin = 0
+    while pin < len(s):
+        if cnt[s[pin]] == tar:
+            del s[pin]
+        else:
+            pin += 1
+    print(''.join(s))
+
+    pass
+
+
+
+# sys.setrecursionlimit(200000)
+if __name__ == "__main__":
+    # size = ii()
+    # for _ in range(size):
+    solve()
